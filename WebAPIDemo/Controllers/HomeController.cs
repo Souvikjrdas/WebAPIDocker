f3bef91b36e51c09ca51c24145d24f0d9bb240e5
Souvik Das <souvik.das02@nagarro.com>
2023-04-17 15:48:50 +0530

    Added WebAPI using Docker project

---
+++ WebAPIDemo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET: api/<HomeController>
+        [HttpGet]
+        //[Route("getAll")]
+        public ActionResult<string> Get()
+        {
+            return Ok("hello world");
+        }
+
+        // GET api/<HomeController>/5
+        [HttpGet("{name}")]
+        public string Get(string name)
+        {
+            return $"hell world from {name}";
+        }
+
+        // POST api/<HomeController>
+        //[HttpPost("[action]/{id:int}/{name}")]
+        //public IActionResult Add([FromRoute] Items item , [FromRoute] Items item1)
+        //{
+        //    LinkedList<string> list = new LinkedList<string>();
+        //    return Ok(); 
+        //}
+
+        //// DELETE api/<HomeController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
